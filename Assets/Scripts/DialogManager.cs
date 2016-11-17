571f3500d84c895d7f36c39fa6518d3324036dff
Boladar <aleksander.szamalek@gmail.com>
2016-11-17 21:44:27 +0100

    initial prototype

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class DialogManager : MonoBehaviour {
+	Dictionary<string,Dialogue> Dialogues;
+	Dialogue CurrentDialogue;
+
+	[SerializeField]
+	public GameObject DialogText;
+	[SerializeField]
+	public GameObject DialogPanel;
+
+	[SerializeField]
+	public List<GameObject> Options;
+
+	// Use this for initialization
+	void Start () {
+		Dialogues = GetComponent<XmlParser> ().Dialogues;
+		ConstructDialog ("1");
+	}
+
+	void Update(){
+		if (DialogPanel.activeSelf == true) {
+			if (Input.GetKeyDown (KeyCode.Alpha1)) {
+				if (CurrentDialogue.Options.Count > 0)
+					ChooseDialogTree (CurrentDialogue.Options [0]);
+			}
+			if(Input.GetKeyDown(KeyCode.Alpha2)){
+				if (CurrentDialogue.Options.Count > 1)
+					ChooseDialogTree (CurrentDialogue.Options [1]);
+			}
+			if(Input.GetKeyDown(KeyCode.Alpha3)){
+				if (CurrentDialogue.Options.Count > 2)
+					ChooseDialogTree (CurrentDialogue.Options [2]);
+			}
+		}
+	}
+
+	public void ChooseDialogTree(Option option)
+	{
+		//zerowanie tekstu opcji
+		foreach (GameObject obj in Options)
+			obj.GetComponent<Text> ().text = "";
+		
+		ConstructDialog (option.ID);
+	}
+
+	public void ConstructDialog(string ID)
+	{
+		CurrentDialogue = Dialogues [ID];
+		ActivateDialogPanel();
+
+		DialogText.GetComponent<Text> ().text = CurrentDialogue.Text;
+
+		int iterator = 0;
+		foreach (GameObject obj in Options) {
+			if (CurrentDialogue.Options.Count > iterator) {
+				Debug.Log ("opcja");
+				obj.GetComponent<Text> ().text = CurrentDialogue.Options [iterator].Text;
+				iterator++;
+			}
+		}
+	}
+
+	public void DeActivateDialogPanel()
+	{
+		DialogPanel.SetActive (false);
+	}
+	public void ActivateDialogPanel()
+	{
+		DialogPanel.SetActive (true);
+	}
+}
---
+++ Assets/Scripts/XmlParser.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Xml;
+using System.Collections.Generic;
+using System.IO;
+
+public class XmlParser : MonoBehaviour {
+
+	public Dictionary<string,Dialogue> Dialogues = new Dictionary<string, Dialogue>();
+
+	// Use this for initialization
+	void Awake () {
+		ReadXml ("data");
+	}
+
+	public void CreateDialogues(XmlReader reader){
+		Dialogue currentDialog = new Dialogue();
+		Dialogues.Add (reader.GetAttribute("id"), currentDialog); 
+
+		XmlReader childReader = reader.ReadSubtree();
+
+		while(childReader.Read())
+		{
+			if(reader.Name == "Text")
+			{
+				childReader.Read();
+				currentDialog.Text = childReader.ReadContentAsString ();
+			}
+			if(childReader.Name == "Option")
+			{
+				Option o = new Option();
+				o.ID = childReader.GetAttribute ("id");
+
+				childReader.Read();
+
+				o.Text = childReader.ReadContentAsString ();
+				currentDialog.Options.Add (o);
+			}
+		}
+	}
+
+	public void ReadXml(string name)
+	{
+		TextAsset textAsset = (TextAsset)Resources.Load(name);
+		XmlTextReader reader = new XmlTextReader( new StringReader(textAsset.text));
+
+		if (reader.ReadToDescendant ("Dialogues")) {
+			if (reader.ReadToDescendant ("Dialogue")) {
+				do{
+					CreateDialogues(reader);
+				}while(reader.ReadToNextSibling("Dialogue"));
+			} else
+				Debug.LogError ("Dialogue is missing!");
+		} else
+			Debug.LogError ("XML file does not containt Dialogues!");
+	}
+
+}
+
+public class Dialogue {
+	public string ID{ get; set; }
+	public string Text{ get; set; }
+	public List<Option> Options{ get; set; }
+
+	public Dialogue(){
+		Options = new List<Option> ();
+	}
+}
+
+public class Option{
+	public string ID{ get; set; }
+	public string Text{ get; set; }
+}
